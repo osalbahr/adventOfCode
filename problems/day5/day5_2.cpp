af522dad184065928df7ed2f656faab1f027028a
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-05 00:40:47 -0500

    Added day5

---
+++ problems/day5/day5_2.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <stack>
+
+#include <cstdio>
+
+#define REPORT( X ) //cout << #X << " = " << ( X ) << endl
+#define REPORT3( X, Y, Z ) //REPORT( X ), REPORT( Y ), REPORT( Z )
+
+using namespace std;
+
+int main( int argc, char *argv[] )
+{
+  string line;
+  int total = 0;
+
+  // array of arrays
+  int n = atoi( argv[ 1 ] );
+  vector<vector<char> > aa( n );
+
+  // get the stacks from top to bottom
+  while ( getline( cin, line ) ) {
+    if ( isdigit( line[ 1 ] ) )
+      break;
+
+    int idx = 1;
+    for ( int i = 0; i < n; i++ ) {
+      char ch = line[ idx + 4 * i ];
+      if ( ch != ' ' )
+        aa[ i ].push_back( ch );
+    }
+  }
+
+  // reverse all arrays
+  for ( auto &a : aa )
+    reverse( a.begin(), a.end() );
+  
+  // put them in an array of stack
+  vector< stack<char> > as( n );
+  for ( int i = 0; i < n; i++ )
+    for ( int j = 0; j < aa[ i ].size(); j++ )
+      as[ i ].push( aa[ i ][ j ] );
+  
+  // // view the stacks
+  // for ( auto s : as )
+  //   cout << s.top() << s.size() << endl;
+
+
+  // remove empty line
+  getline( cin, line );
+
+  // actions
+  while ( getline( cin, line ) ) {
+    int moves, src, dest;
+    sscanf( line.c_str(), "move %d from %d to %d", &moves, &src, &dest );
+    src--;
+    dest--;
+    // put them in temp first, to reverse them
+    stack<char> temp;
+    for ( int i = 0; i < moves; i++ ) {
+      temp.push( as[ src ].top() );
+      as[ src ].pop();
+    }
+    while ( !temp.empty() ) {
+      as[ dest ].push( temp.top() );
+      temp.pop();
+    }
+  }
+
+  for ( auto s : as )
+    cout << s.top();
+  cout << endl;
+}
