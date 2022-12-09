455c13bc99995970ae10ef6848167636b4e0d728
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-09 01:17:48 -0500

    More general design

--- problems/day9/day9.cpp
+++ problems/day9/day9.cpp
@@ -19,6 +19,21 @@ static bool adjacent( pair<int,int> a, pair<int, int> b )
   return abs( a.x - b.x ) <= 1 && abs( a.y - b.y ) <= 1;
 }
 
+// Let tail follow head
+static pair<int,int> getNew( pair<int,int> t, pair<int,int> h )
+{
+  // How far away are they?
+  int xDiff = h.x - t.x;
+  int yDiff = h.y - t.y;
+
+  // What is the sign?
+  int xSign = xDiff == 0 ? 0 : xDiff / abs( xDiff );
+  int ySign = yDiff == 0 ? 0 : yDiff / abs( yDiff );
+
+  // I think this follows all rules
+  return mp( t.x + xSign, t.y + ySign );
+}
+
 int main()
 {
   string line;
@@ -59,9 +74,9 @@ int main()
       // Does tail need to move?
       pair<int,int> newH = mp( h.x + xDiff, h.y + yDiff );
       if ( !adjacent( t, newH ) ) {
-        t = h;
-        // if ( locations.count( t ) == 0 )
-        //   cout << "new: " << t.x << "," << t.y << endl;
+        // t = h; // cannot generalize
+        // Apply rules
+        t = getNew( t, newH );
 
         locations.insert( t );
       }
