87b1c276aaf55b04012b8dd3d88014d9e69b42a7
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-11 01:27:37 -0500

    Added Day 11 Part 1

--- problems/day11/day11.cpp
+++ problems/day11/day11.cpp
@@ -3,6 +3,7 @@
 #include <set>
 #include <map>
 #include <sstream>
+#include <algorithm>
 
 #include <cstdio>
 
@@ -19,12 +20,66 @@ typedef struct MonketTag {
 
 Monkey **monkeys = (Monkey **)malloc( 8 * sizeof( Monkey ) );
 
+// Where to insert
+int idx = 0;
+
+// Keep track of inspections here
+vector<int> inspections( 8 );
+
+static int applyOperation( int worryLevel, string operation )
+{
+  int operand;
+  if ( operation[ 1 ] == 'o' ) {
+    operand = worryLevel;
+  } else {
+    sscanf( operation.c_str() + 1, "%d", &operand );
+  }
+
+  if ( operation[ 0 ] == '+' ) {
+    worryLevel += operand;
+  } else {
+    worryLevel *= operand;
+  }
+
+  return worryLevel;
+}
+
+static void doRound()
+{
+  for ( int i = 0; i < idx; i++ ) {
+    // cout << "Monkey " << i << endl;
+
+    Monkey *monkey = monkeys[ i ];
+    vector<int> *items = monkey->items;
+    inspections[ i ] += items->size();
+    cout << inspections[ i ] << endl;
+    for ( auto worryLevel : *items ) {
+
+      // cout << "Item " << worryLevel << endl;;
+      worryLevel = applyOperation( worryLevel, *monkey->operation );
+      // cout << "After " << worryLevel << endl;
+
+      worryLevel /= 3;
+      // cout << "Bored " << worryLevel << endl;
+
+      // cout << "True idx: " << monkey->trueIdx << endl;
+      int throwIdx = worryLevel % monkey->divideTest == 0 ?
+                     monkey->trueIdx :
+                     monkey->falseIdx;
+
+      // cout << "Throwing to " << throwIdx << endl;
+      monkeys[ throwIdx ]->items->push_back( worryLevel );
+    }
+    items->clear();
+
+    cout << endl;
+  }
+}
 
 int main()
 {
   string line;
   
-  int idx = 0;
   while ( getline( cin, line ) ) {
     stringstream ss( line );
     Monkey *monkey = (Monkey *)malloc( sizeof( Monkey ) );
@@ -91,11 +146,22 @@ int main()
     idx++;
   }
 
-  for ( int i = 0; i < idx; i++ ) {
-    cout << i << endl;
-    for ( auto a : *monkeys[ i ]->items ) {
-      cout << a << " ";
+
+  int rounds = 20;
+  for ( int round = 1; round <= rounds; round++ ) {
+    doRound();
+
+    cout << "Round summary: " << round << endl;
+    for ( int i = 0; i < idx; i++ ) {
+      cout << "Monkey " << i << ": ";
+      for ( int item : *monkeys[ i ]->items )
+        cout << item << ", ";
+      cout << endl;
     }
-    cout << endl;
   }
+
+  // Find top 2
+  sort( inspections.begin(), inspections.end() );
+  reverse( inspections.begin(), inspections.end() );
+  cout << inspections[ 0 ] * inspections[ 1 ] << endl;
 }
