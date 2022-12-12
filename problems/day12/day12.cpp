8d83167e0cc95bb6df75e514c59040022a1cc456
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-12 02:46:46 -0500

    Removed commented-out lines (actually)

--- problems/day12/day12.cpp
+++ problems/day12/day12.cpp
@@ -27,70 +27,63 @@ void printPath( const vector<pair<int,int>> &path )
 
 void findPaths( vector<pair<int,int>> path )
 {
-  // printPath( path );
-  // cout << "1" << endl;
-  // cout << "2" << endl;
-  // Try all directions
-  // cout << "c" << endl;
   pair<int,int> start = path[ path.size() - 1 ];
   char currentChar =  path.size() == 1 ?
                       'a' :
                       grid[ start.first ][ start.second ];
-
-  // REPORT( currentChar );
-  // cout << "c2" << endl;
+  
   for ( int i = -1; i <= 1; i++ )
     for ( int j = -1; j <= 1; j++ ) {
       if ( i != 0 && j != 0 )
         continue;
       
-      // REPORT( i );
-      // REPORT( j );
+      
+      
 
-      // cout << "ij" << endl;
-      // cout << i << endl;
-      // cout << j << endl;
+      
+      
+      
 
       int newX = start.first + i;
       int newY = start.second + j;
-      // cout << "what" << endl;
+      
       if ( newX < 0 || newX >= grid.size() )
         continue;
       if ( newY < 0 || newY >= grid[ 0 ].size() )
         continue;
 
-      // cout << "yes" << endl;
+      
 
       if ( path.size() > 1 ) {
-        // cout << "3" << endl;
+        
         pair<int,int> last = path[ path.size() - 1 ];
-        // cout << "4" << endl;
+        
         if ( newX == last.first && newY == last.second )
           continue;
       }
       
-      // cout << "new" << endl;
+      
       char newChar = grid[ newX ][ newY ];
-      // bool end = false;
+      
       if ( newChar == 'E' ) {
         newChar = 'z';
       }
 
-      // REPORT( newChar );
-      // if ( newChar == 'b' && newX == 4 && newY == 1 ) {
-      //   exit( 1 );
-      // }
-      // cout << "new2" << endl;
+      
+      
+      
+      
+      
 
       pair<int,int> newPos = { newX, newY };
       if ( newChar > currentChar + 1 ) {
-        // cout << "newChar > currentChar" << endl;
+        
         continue;
       }
 
-      // cout << "push" << endl;
-      // cout << newPos.x << "," << newPos.y << endl;
-      // Do not go in circles
+      
+      
+      
       bool found = false;
       for ( auto prev : path ) {
         if ( prev == newPos ) {
@@ -101,23 +94,23 @@ void findPaths( vector<pair<int,int>> path )
       if ( found )
         continue;
 
-      // cout << "before:" << endl;
-      // REPORT( currentChar );
-      // REPORT( grid[ path[ path.size() - 1 ].x ][ path[ path.size() - 1 ].y ] );
-      // printPath( path );
+      
+      
+      
+      
 
-      // cout << "pu" << endl;
-      // printPath( path );
-      // cout << newPos.x << newPos.y << endl;
+      
+      
+      
       path.push_back( newPos );
-      // printPath( path );
-      // cout << "sh" << endl;
-      // bool me = path.size() == 2 && path[ 0 ] == make_pair(0, 0) && path[ 1 ] == make_pair(0, 1);
+      
+      
+      
 
-      // printPath( path );
-      // REPORT( newChar );
-      // cout << "after^" << endl;
-      // cout << "pop" << endl;
+      
+      
+      
+      
       
       if ( paths.insert( path ).second ) {
         if ( paths.size() % 100000 == 0 ) {
@@ -125,31 +118,31 @@ void findPaths( vector<pair<int,int>> path )
           cout << path.size() << endl;
           printPath( path );
         }
-        // printPath( path );
-        // cout << "6" << endl;
+        
+        
 
-        // if ( me )
-        //   cout << "recurse" << endl;
+        
+        
         findPaths( path );
-        // if ( me )
-        //   cout << "po" << endl;
-        // printPath( path );
+        
+        
+        
         path.pop_back();
-        // printPath( path );
-        // if ( me )
-        //   cout << "op" << endl;
-        // if ( me )
-        //   cout << "SOS" << endl;
+        
+        
+        
+        
+        
       } else {
         return;
       }
-      // cout << "7" << endl;
+      
     }
 
-  // Never reached
-  // cout << "erase1" << endl;
-  // paths.erase( path );
-  // cout << "erase2" << endl;
+  
+  
+  
+  
 }
 
 int main()
@@ -168,7 +161,7 @@ int main()
       break;
 
     for ( int j = 0; j < grid[ 0 ].size(); j++ ) {
-      // cout << grid[ i ][ j ];
+      
       if ( foundS && foundE )
         break;
       
@@ -185,35 +178,35 @@ int main()
   vector<pair<int,int>> path;
   path.push_back( S );
   paths.insert( path );
-  // cout << "find" << endl;
+  
   findPaths( path );
-  // cout << "no find" << endl;
+  
 
   vector<int> lengths;
-  // cout << "auto" << endl;
+  
   for ( auto path : paths ) {
-    // printPath( path );
-    // cout << "auto1.5" << endl;
+    
+    
     pair<int,int> last = path[ path.size() - 1 ];
     if ( grid[ last.x ][ last.y ] == 'E' ) {
       lengths.push_back( path.size() );
-      // REPORT( path.size() );
-      // if ( path.size() == 26 ) {
-      //   // printPath( path );
-      //   for ( auto p : path ) {
-      //     printf( "(%d,%d) = %c\n", p.x, p.y, grid[ p.x ][ p.y ] );
-      //   }
-      //   exit( 1 );
-      // }
+      
+      
+      
+      
+      
+      
+      
+      
     }
-    // cout << path.size() << endl;
+    
   }
-  // cout << "auto2" << endl;
+  
   
   sort( lengths.begin(), lengths.end() );
   cout << lengths[ 0 ] - 1 << endl;
 
-  // for ( auto path : paths ) {
-  //   printPath( path );
-  // }
+  
+  
+  
 }
