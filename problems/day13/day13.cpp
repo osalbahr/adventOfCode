7cd6031ef2e2917db012a7bfd33c5c6deaff7231
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-13 01:26:11 -0500

    Added Day 13 Part 1

---
+++ problems/day13/day13.cpp
@@ -0,0 +1,156 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <map>
+#include <sstream>
+
+#include <cstdio>
+
+using namespace std;
+
+#define REPORT( X ) cout << #X << " = " << ( X ) << endl
+
+vector<string> tokenize( string s )
+{
+  vector<string> tokens;
+  int idx = 1; // skip leading [
+  string token = "";
+  while( idx < s.size() ) {
+    if ( idx == s.size() - 1 ) {
+      if ( !token.empty() ) {
+        tokens.push_back( token );
+        //REPORT( token );
+        token = "";
+      }
+      break;
+    }
+    if ( s[ idx ] == '[' ) {
+      cout << "// gobble it up" << endl;
+      token += '[';
+      // tokens.push_back( token );
+      // //REPORT( token );
+
+      int opening = 1;
+      idx++;
+      while( true ) {
+        switch( s[ idx ] ) {
+          case '[':
+          opening++;
+          break;
+          case ']':
+          opening--;
+          break;
+        }
+        token += s[ idx ];
+        if ( opening == 0 )
+          break;
+        idx++;
+      }
+      cout << "gobbled" << endl;
+      REPORT( token );
+      tokens.push_back( token );
+      //REPORT( token );
+      token = "";
+      idx++;
+      continue;
+    }
+    if ( s[ idx ] == ',' ) {
+      if ( !token.empty() ) {
+        tokens.push_back( token );
+        token = "";
+      }
+      idx++;
+      continue;
+    }
+
+    token += s[ idx ];
+    idx++;
+  }
+
+  cout << "tokenized" << endl;
+  REPORT( s );
+  for ( auto token : tokens )
+    REPORT( token );
+  return tokens;
+}
+
+// -1 or 0 or 1
+int matchVec( vector<string> v1, vector<string> v2 )
+{
+  v1.size() == 0 ? cout << "v1 empty" << endl
+                  : REPORT( v1[ 0 ] );
+  v2.size() == 0 ? cout << "v2 empty" << endl
+                  : REPORT( v2[ 0 ] );
+  for ( int i = 0; i < v1.size(); i++ ) {
+    if ( i >= v2.size() )
+      return 1;
+    
+    string s1 = v1[ i ];
+    string s2 = v2[ i ];
+    REPORT( s1 );
+    REPORT( s2 );
+
+    stringstream ss1( s1 );
+    stringstream ss2( s2 );
+
+    int n1, n2;
+    if ( ss1 >> n1 && ss2 >> n2 ) {
+      if ( n1 < n2 )
+        return -1;
+      if ( n1 > n2 )
+        return 1;
+    } else {
+      // Promote them
+      if ( s1[ 0 ] != '[' ) {
+        s1 = '[' + s1 + ']';
+      }
+      if ( s2[ 0 ] != '[' )
+        s2 = '[' + s2 + ']';
+      cout << "Recursive{" << endl;
+      REPORT( s1 );
+      REPORT( s2 );
+      cout << "}Recursive" << endl;
+      int val = matchVec( tokenize( s1 ), tokenize( s2 ) );
+      if ( val != 0 )
+        return val;
+    }
+  }
+
+  // Did left run out or are they equal
+  cout << "RUNNING" << endl;
+  return v1.size() < v2.size() ? -1 : 0;
+}
+
+int match( string s1, string s2 )
+{
+  // Promote them
+  if ( s1[ 0 ] != '[' ) {
+    s1 = '[' + s1 + ']';
+  }
+  if ( s2[ 0 ] != '[' )
+    s2 = '[' + s2 + ']';
+
+  return matchVec( tokenize( s1 ), tokenize( s2 ) );
+}
+
+int main()
+{
+  string line1, line2;
+  
+  int total = 0;
+  int idx = 1;
+  while ( getline( cin, line1 ) && getline( cin, line2 ) ) {
+    cout << line1 << endl;
+    cout << line2 << endl;
+    bool isMatch = match( line1, line2 ) == -1;
+    total += idx * isMatch;
+    if ( isMatch )
+      cout << idx << " matched" << endl;
+    else
+      cout << idx << " not matched" << endl;
+
+    getline( cin, line1 );
+    idx++;
+  }
+  cout << total << endl;
+}
