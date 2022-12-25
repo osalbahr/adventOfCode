14b535045d230c4eea9cc9ae977532b0435f49f3
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-26 00:50:48 +0300

    Added parsing

--- problems/day16/day16.cpp
+++ problems/day16/day16.cpp
@@ -25,17 +25,80 @@ using namespace std;
 #define forn( X ) \
 for ( int ii = 0; ii < ( X ); ii++ )
 
+typedef struct {
+  string name;
+  int rate;
+  vector<string> *list;
+} Valve;
 
-static void solve()
+map<string,Valve*> valves;
+
+static Valve* parseLine( string line )
 {
+  // Make the valve
+  Valve *valve = new Valve;
 
+  // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+  stringstream ss( line );
+
+  string discardStr;
+  char discardChar = '\0';
+
+  // Valve
+  forn( 1 )
+    ss >> discardStr;
+  // AA
+  ss >> valve->name;
+  // has flow
+  forn( 2 )
+    ss >> discardStr; 
+  // rate=
+  forn( 5 )
+    ss >> discardChar;
+  //0
+  ss >> valve->rate;
+  // ; tunnels lead to valves
+  forn( 5 )
+    ss >> discardStr;
+  // Fill it in
+  valve->list = new vector<string>;
+  char ch1, ch2;
+  while( ss >> ch1 && ss >> ch2 ) {
+    string name;
+    name += ch1;
+    name += ch2;
+    valve->list->push_back( name );
+    // Maybe comma
+    ss >> discardChar;
+  }
+
+  return valve;
+}
+
+static void printValves( FILE *fp )
+{
+  for ( auto itemPair : valves ) {
+    auto valve = itemPair.second;
+    // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    fprintf( fp, "Valve %s has flow rate=%d; tunnels lead to valves ", valve->name.c_str(), valve->rate );
+    int i;
+    for ( i = 0; i < valve->list->size() - 1; i++ )
+      fprintf( fp, "%s, ", (*valve->list)[ i ].c_str() );
+    fprintf( fp, "%s\n", (*valve->list)[ i ].c_str() );
+  }
 }
 
 int main()
 {
   string line;
+
   while ( getline( cin, line ) ) {
-    cout << line << endl;
+    Valve *valve = parseLine( line );
+    valves[ valve->name ] = valve;
   }
-  solve();
+
+  // Check parsing
+  FILE *parse = fopen( "PARSE", "w" );
+  printValves( parse );
+  fclose( parse );
 }
