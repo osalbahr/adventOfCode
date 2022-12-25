ed1220bd6f631cbe2e6f4338c5e263c757564879
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-26 01:01:29 +0300

    Now parsing is identical to input format

--- problems/day16/day16.cpp
+++ problems/day16/day16.cpp
@@ -28,10 +28,19 @@ for ( int ii = 0; ii < ( X ); ii++ )
 typedef struct {
   string name;
   int rate;
-  vector<string> *list;
+  vector<string> list;
 } Valve;
 
+// Only for parsing
+vector<Valve*> parsingList;
+
+// name lookup
 map<string,Valve*> valves;
+
+typedef pair<string,string> ps;
+
+// Note: distance is bi-directional
+map<ps,int> distance;
 
 static Valve* parseLine( string line )
 {
@@ -61,13 +70,12 @@ static Valve* parseLine( string line )
   forn( 5 )
     ss >> discardStr;
   // Fill it in
-  valve->list = new vector<string>;
   char ch1, ch2;
   while( ss >> ch1 && ss >> ch2 ) {
     string name;
     name += ch1;
     name += ch2;
-    valve->list->push_back( name );
+    valve->list.push_back( name );
     // Maybe comma
     ss >> discardChar;
   }
@@ -77,14 +85,18 @@ static Valve* parseLine( string line )
 
 static void printValves( FILE *fp )
 {
-  for ( auto itemPair : valves ) {
-    auto valve = itemPair.second;
+  for ( Valve* valve : parsingList ) {
     // Valve AA has flow rate=0; tunnels lead to valves DD, II, BB
+    if ( valve->list.size() == 1 ) {
+      fprintf( fp, "Valve %s has flow rate=%d; tunnel leads to valve %s\n", valve->name.c_str(), valve->rate, valve->list[ 0 ].c_str() );
+      continue;
+    }
+  
     fprintf( fp, "Valve %s has flow rate=%d; tunnels lead to valves ", valve->name.c_str(), valve->rate );
     int i;
-    for ( i = 0; i < valve->list->size() - 1; i++ )
-      fprintf( fp, "%s, ", (*valve->list)[ i ].c_str() );
-    fprintf( fp, "%s\n", (*valve->list)[ i ].c_str() );
+    for ( i = 0; i < valve->list.size() - 1; i++ )
+      fprintf( fp, "%s, ", valve->list[ i ].c_str() );
+    fprintf( fp, "%s\n", valve->list[ i ].c_str() );
   }
 }
 
@@ -95,6 +107,7 @@ int main()
   while ( getline( cin, line ) ) {
     Valve *valve = parseLine( line );
     valves[ valve->name ] = valve;
+    parsingList.push_back( valve );
   }
 
   // Check parsing
