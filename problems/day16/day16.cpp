9429ab3da1c28da382804e37bc2c63cc8ba05047
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-26 00:41:47 +0300

    Removed unused geometry template

--- problems/day16/day16.cpp
+++ problems/day16/day16.cpp
@@ -20,39 +20,10 @@ using namespace std;
 #define REPORT( X ) cout << #X " = " << X << endl
 // like echo -n
 #define REPORTN( X ) cout << #X << " = " << ( X ) << ", "
-#define REPORTP( M ) fprintf( stdout, "%s = (%d,%d)\n", #M, M.row, M.col )
 
 // Aliasing
 #define forn( X ) \
 for ( int ii = 0; ii < ( X ); ii++ )
-
-// Point syntactic sugar
-typedef pair<int,int> pi;
-// Grid
-#define row first
-#define col second
-// Raw points
-// #define x first
-// #define y second
-#define mp make_pair
-
-// Apadted from
-// https://stackoverflow.com/a/21956177/18633613
-// (https://stackoverflow.com/questions/21956119/add-stdpair-with-operator)
-pi operator+(const pi& a, const pi& b) {
-    return make_pair(a.first + b.first, a.second + b.second);
-}
-
-// Scaling
-pi operator*(const int a, const pi& p) {
-    return {a * p.first, a * p.second};
-}
-
-
-
-
-// Add program state (global variables) here
-
 
 
 static void solve()
