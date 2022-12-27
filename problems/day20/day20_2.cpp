f184866fcf0934a5b2aad4ebfa5ea230ca727d83
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-27 21:13:50 +0300

    [Debugging] pointer being freed was not allocated (see err_report)

--- problems/day20/day20_2.cpp
+++ problems/day20/day20_2.cpp
@@ -1,24 +1,180 @@
+#define DEBUG
+
 #include <iostream>
 #include <vector>
 #include <set>
 #include <map>
+#include <list>
+#include <algorithm>
 
 #include <cstdio>
+#include <cassert>
 
 using namespace std;
 
-void solve()
+#define REPORT( X ) cout << #X << " = " << (X) << endl
+
+// Aliasing
+#define forn( X ) \
+for ( int i_ = 0; i_ < ( X ); i_++ )
+
+#define KEY 811589153
+
+int mixCount = 10;
+
+static void printList( const list<long>& numbers, FILE *fp )
 {
-  
+  for ( long n : numbers ) {
+    fprintf( fp, "%ld\n", n );
+  }
 }
 
-int main()
+static void printListReverse( const list<long>& numbers, FILE *fp )
 {
-  int t;
-  cin >> t;
-    
-  while ( t-- ) {
-    
-    solve();
+  for ( auto it = numbers.rbegin(); it != numbers.rend(); it++ )
+    fprintf( fp, "%ld\n", *it );
+}
+
+#ifdef DEBUG
+static void printListCommas( list<long> numbers, FILE *fp )
+{
+  auto node = numbers.begin();
+  fprintf( fp, "%ld", *node );
+
+  while ( (++node) != numbers.end() )
+    fprintf( fp, ", %ld", *node );
+
+  fprintf( fp, "\n" );
+}
+#endif
+
+static void mixNode( list<long>& numbers, list<long>::iterator node )
+{
+  long n = *node;
+  if ( n == 0 )
+    return;
+  
+  int moves = abs( n ) % ( numbers.size() - 1 );
+  if ( moves == 0 )
+    return;
+
+  if ( n > 0 ) {
+    forn( moves ) {
+      auto pos = numbers.erase( node );
+      if ( pos == numbers.end() ) {
+        pos = numbers.begin();
+      }
+      pos++;
+      node = numbers.insert( pos, n );
+    }
+  } else { // n < 0
+    forn( moves ) {
+      auto pos = numbers.erase( node );
+      pos--;
+      if ( pos == numbers.begin() ) {
+        pos = numbers.end();
+      }
+      node = numbers.insert( pos, n );
+    }
   }
+}
+
+static void mix( list<long>& numbers, const vector< list<long>::iterator >& initial )
+{  
+  for ( auto node : initial )
+    mixNode( numbers, node );
+}
+
+static int getZeroIdx( const list<long>& numbers )
+{
+  int idx = 0;
+  for ( auto it = numbers.begin(); it != numbers.end(); it++ ) {
+    if ( *it == 0 )
+      return idx;
+    idx++;
+  }
+  cerr << "Zero node not found" << endl;
+  exit( 1 );
+}
+
+int main( int argc, char *argv[] )
+{
+  if ( argc == 2 )
+    mixCount = atoi( argv[ 1 ] );
+
+  list<long> numbers;
+
+  long n;
+  while ( cin >> n )
+    numbers.push_back( KEY * n );
+
+  // Check it
+  fclose( fopen( "PARSE", "w" ) );
+  FILE *parse = fopen( "PARSE", "w" );
+  printList( numbers, parse );
+  fclose( parse );
+
+  // Check it reversed
+  fclose( fopen( "REVERSE", "w" ) );
+  FILE *rev = fopen( "REVERSE", "w" );
+  printListReverse( numbers, rev );
+  fclose( rev );
+
+  vector< list<long>::iterator > initial;
+  for ( auto node = numbers.begin(); node != numbers.end(); node++ )
+    initial.push_back( node );
+
+#ifdef DEBUG
+  FILE *out = fopen( "out", "w" );
+  fprintf( out, "Initial arrangement:\n" );
+  printListCommas( numbers, out );
+  fprintf( out, "\n" );
+#endif
+
+  cout << "Mixing ... " << flush;
+  // Mix them
+  forn( mixCount ) {
+    cout << i_ + 1 << " " << flush;
+    mix( numbers, initial );
+#ifdef DEBUG
+  fprintf( out, "After %d rounds of mixing:\n", i_ + 1 );
+  printListCommas( numbers, out );
+  fprintf( out, "\n" );
+#endif
+  }
+  cout << "Done" << endl;
+
+#ifdef DEBUG
+  fclose( out );
+#endif
+
+  // printListCommas( numbers, stdout, NULL );
+  
+  int zeroIdx = getZeroIdx( numbers );
+  REPORT( zeroIdx );
+
+  // Dump it all in a vector
+  vector<long> numbersVec;
+  for ( long n : numbers )
+    numbersVec.push_back( n );
+
+  assert( numbersVec.size() == numbers.size() );
+  int size = numbers.size();
+
+  REPORT( numbersVec[ zeroIdx ] );
+
+  long n1, n2, n3;
+  REPORT( ( n1 = numbersVec[ ( zeroIdx + 1000 ) % size ] ) );
+  REPORT( ( n2 = numbersVec[ ( zeroIdx + 2000 ) % size ] ) );
+  REPORT( ( n3 = numbersVec[ ( zeroIdx + 3000 ) % size ] ) );
+
+  long total = n1 + n2 + n3;
+  REPORT( total );
+
+  if ( KEY == 1 )
+    assert( total == 3 || total == 7225 );
+  else
+    switch( mixCount )
+      case 1:
+        assert( total == 2434767459 + 1623178306 + 811589153 );
 }
