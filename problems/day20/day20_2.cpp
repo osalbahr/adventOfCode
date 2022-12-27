c8af224d2322dea68ea6cec6962887ac18bb3815
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2022-12-27 18:18:46 -0500

    Added Day 20 Part 2

--- problems/day20/day20_2.cpp
+++ problems/day20/day20_2.cpp
@@ -1,4 +1,4 @@
-#define DEBUG
+// #define DEBUG
 
 #include <iostream>
 #include <vector>
@@ -15,10 +15,14 @@ using namespace std;
 #define REPORT( X ) cout << #X << " = " << (X) << endl
 
 // Aliasing
-#define forn( X ) \
-for ( int i_ = 0; i_ < ( X ); i_++ )
+#define forn( X )\
+assert( (X) >= 0 );\
+for ( size_t i_ = 0; i_ < size_t(X); i_++ )\
 
-#define KEY 811589153
+
+int key = 811589153;
+#define KEY key
+// #define flush ""
 
 int mixCount = 10;
 
@@ -36,8 +40,11 @@ static void printListReverse( const list<long>& numbers, FILE *fp )
 }
 
 #ifdef DEBUG
-static void printListCommas( list<long> numbers, FILE *fp )
+static void printListCommas( list<long> numbers, FILE *fp, long *n )
 {
+  if ( n != NULL )
+    fprintf( fp, "%ld moves:\n", *n );
+
   auto node = numbers.begin();
   fprintf( fp, "%ld", *node );
 
@@ -48,15 +55,21 @@ static void printListCommas( list<long> numbers, FILE *fp )
 }
 #endif
 
+#ifdef DEBUG
+FILE *out;
+#endif
+
 static list<long>::iterator mixNode( list<long>& numbers, list<long>::iterator node )
 {
   long n = *node;
   if ( n == 0 )
     return node;
-  
+
+#ifndef SLOW
   int moves = abs( n ) % ( numbers.size() - 1 );
-  if ( moves == 0 )
-    return node;
+#else
+  long moves = abs( n );
+#endif
 
   if ( n > 0 ) {
     forn( moves ) {
@@ -68,13 +81,20 @@ static list<long>::iterator mixNode( list<long>& numbers, list<long>::iterator n
       node = numbers.insert( pos, n );
     }
   } else { // n < 0
+    if ( node == numbers.begin() ) {
+      moves++;
+      moves %= numbers.size() - 1;
+    }
     forn( moves ) {
       auto pos = numbers.erase( node );
       pos--;
       if ( pos == numbers.begin() ) {
         pos = numbers.end();
       }
+
       node = numbers.insert( pos, n );
+
+      // printListCommas( numbers, out, &n );
     }
   }
 
@@ -85,8 +105,19 @@ vector< list<long>::iterator > initial;
 static void mix( list<long>& numbers, vector< list<long>::iterator > nodes )
 {
   initial.clear();
-  for ( auto node : nodes )
+  for ( auto node : nodes ) {
+#ifdef DEBUG
+    long n = *node;
+    cout << "(" << *node << flush;
+#endif
     initial.push_back( mixNode( numbers, node ) );
+#ifdef DEBUG
+    cout << ") " << flush;
+    printListCommas( numbers, out, &n );
+    fprintf( out, "\n" );
+    fflush( out );
+#endif
+  }
 }
 
 static int getZeroIdx( const list<long>& numbers )
@@ -103,23 +134,31 @@ static int getZeroIdx( const list<long>& numbers )
 
 int main( int argc, char *argv[] )
 {
-  if ( argc == 2 )
+#ifdef DEBUG
+  out = fopen( "out", "w" );
+#endif
+
+  if ( argc == 2 ) {
     mixCount = atoi( argv[ 1 ] );
+    if ( mixCount == -1 ) {
+      mixCount = 1;
+      key = 1;
+    }
+  }
 
   list<long> numbers;
 
   long n;
-  while ( cin >> n )
+  while ( cin >> n ) {
     numbers.push_back( KEY * n );
+  }
 
   // Check it
-  fclose( fopen( "PARSE", "w" ) );
   FILE *parse = fopen( "PARSE", "w" );
   printList( numbers, parse );
   fclose( parse );
 
   // Check it reversed
-  fclose( fopen( "REVERSE", "w" ) );
   FILE *rev = fopen( "REVERSE", "w" );
   printListReverse( numbers, rev );
   fclose( rev );
@@ -128,9 +167,8 @@ int main( int argc, char *argv[] )
     initial.push_back( node );
 
 #ifdef DEBUG
-  FILE *out = fopen( "out", "w" );
   fprintf( out, "Initial arrangement:\n" );
-  printListCommas( numbers, out );
+  printListCommas( numbers, out, NULL );
   fprintf( out, "\n" );
   fflush( out );
 #endif
@@ -141,8 +179,8 @@ int main( int argc, char *argv[] )
     cout << i_ + 1 << " " << flush;
     mix( numbers, initial );
 #ifdef DEBUG
-  fprintf( out, "After %d rounds of mixing:\n", i_ + 1 );
-  printListCommas( numbers, out );
+  fprintf( out, "After %ld rounds of mixing:\n", i_ + 1 );
+  printListCommas( numbers, out, NULL );
   fprintf( out, "\n" );
   fflush( out );
 #endif
@@ -176,10 +214,14 @@ int main( int argc, char *argv[] )
   long total = n1 + n2 + n3;
   REPORT( total );
 
-  if ( KEY == 1 )
+  if ( KEY == 1 && mixCount == 1 )
     assert( total == 3 || total == 7225 );
   else
-    switch( mixCount )
+    switch( mixCount ) {
       case 1:
         assert( total == 2434767459 + 1623178306 + 811589153 );
+        break;
+      case 10:
+        assert( total == 1623178306 || total == 548634267428 );
+    }
 }
