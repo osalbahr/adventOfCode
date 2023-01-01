9051e07ee795b45eaaad0160363d6238dc713a5d
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-01-01 21:32:10 +0300

    [Done] Added Day 18 Part 2

--- problems/day18/day18_2.cpp
+++ problems/day18/day18_2.cpp
@@ -13,6 +13,8 @@ using namespace std;
 // Like echo -n
 #define REPORTN( X ) cout << #X << " = " << ( X ) << ", "
 
+#define REPORTC( C ) printf( "%s = (%d,%d,%d)\n", #C, (C).x, (C).y, (C).z );
+
 typedef struct {
   int x;
   int y;
@@ -25,11 +27,18 @@ bool operator<(const Cube& a, const Cube& b) {
     return tie(a.x, a.y, a.z) < tie(b.x, b.y, b.z);
 }
 
+Cube operator+( const Cube& l, const Cube& r ) {
+  int x = l.x + r.x;
+  int y = l.y + r.y;
+  int z = l.z + r.z;
+  return { x, y, z };
+}
+
 // A side is a midpoint of two cubes
 typedef struct {
-  double x;
-  double y;
-  double z;
+  float x;
+  float y;
+  float z;
 } Side;
 
 // Adapted from
@@ -41,14 +50,114 @@ bool operator<(const Side& a, const Side& b) {
 set<Cube> cubes;
 set<Side> sides;
 
+int minX, maxX;
+int minY, maxY;
+int minZ, maxZ;
+set<Cube> lavaCubes;
+
 static void printCubes( FILE *fp )
 {
   for ( Cube cube : cubes )
-    fprintf( fp, "%d,%d,%d\n", cube.x, cube.y, cube.z );
+    fprintf( fp, "%d,%d,%d\n", cube.x - 1, cube.y - 1, cube.z - 1 );
 }
 
-static void removeSidesBetweenCubes()
+// static void printSides()
+// {
+//   for ( Side s : sides ) {
+//     printf( "(%.1f,%.1f,%.1f)\n", s.x - 1, s.y - 1, s.z - 1 );
+//   }
+// }
+
+static Cube getOppositeCube( Side side ) {
+  float diffs[] = { -0.5, 0.5 };
+  bool isX = side.x != (int)side.x;
+  bool isY = side.y != (int)side.y;
+  bool isZ = side.z != (int)side.z;
+  for ( float diff : diffs ) {
+    Cube cube = { int(side.x + isX * diff),
+                  int(side.y + isY * diff),
+                  int(side.z + isZ * diff) };
+    
+    if ( cubes.count( cube ) > 0 ) {
+      return {  int(side.x + isX * -diff),
+                int(side.y + isY * -diff),
+                int(side.z + isZ * -diff) };
+    }
+  }
+
+  cerr << "Not found" << endl;
+  exit( 1 );
+}
+
+static bool isLavaSide( Side side ) {
+  // Look around based on orientation
+  Cube oppositeCube = getOppositeCube( side );
+
+  // REPORT( lavaCubes.count( oppositeCube ) > 0 );
+  return lavaCubes.count( oppositeCube ) > 0;
+}
+
+static void dfsLava( Cube lava )
 {
+  // Off limits
+  if (   lava.x > maxX + 1 || lava.x < minX - 1
+      || lava.y > maxY + 1 || lava.y < minY - 1
+      || lava.z > maxZ + 1 || lava.z < minZ - 1 )
+    return;
+  
+  // Can't be both lava and non-lava
+  if ( cubes.count( lava ) > 0 )
+    return;
+
+  // Already inserted
+  if ( ! lavaCubes.insert( lava ).second )
+    return;
+  
+  // Start dfs
+  Cube differs[] = {
+    { -1, 0, 0 }, { 1, 0, 0 },
+    { 0, -1, 0 }, { 0, 1, 0 },
+    { 0, 0, -1 }, { 0, 0, 1 }
+  };
+  for ( Cube differ : differs ) {
+    dfsLava( lava + differ );
+  }
+}
+
+// Mark the corner as a lava cube
+// DFS from there
+static void populateLavaCubes()
+{
+  // Mark the corner as a lava cube
+  Cube cube = *cubes.begin();
+  minX = maxX = cube.x;
+  minY = maxY = cube.y;
+  minZ = maxZ = cube.z;
+  for ( Cube cube : cubes ) {
+    minX = min( minX, cube.x );
+    maxX = max( maxX, cube.x );
+
+    minY = min( minY, cube.y );
+    maxY = max( maxY, cube.y );
+
+    minZ = min( minZ, cube.z );
+    maxZ = max( maxZ, cube.z );
+  }
+
+  Cube lava = { minX - 1, minY - 1, minZ - 1 };
+  dfsLava( lava );
+}
+
+// Keep only valid sides
+static void filterSides()
+{
+  // cout << "populateLavaCubes ... " << flush;
+  populateLavaCubes();
+  // cout << "Done" << endl;
+
+  assert( lavaCubes.size() );
+
+  // Filter out sides not exposed to lava
   for ( auto it = sides.begin(); it != sides.end(); ) {
     Side side = *it;
     assert( side.x > 0 );
@@ -80,26 +189,37 @@ static void removeSidesBetweenCubes()
       exit( 1 );
     }
 
-    // Count only non-squeezed sides
+    // Not exposed to air
     if ( countA > 0 && countB > 0 ) {
       sides.erase( it++ );
-    } else {
-      it++;
+      continue;
     }
+    
+    // Not exposed to lava
+    if ( !isLavaSide( side ) ) {
+      sides.erase( it++ );
+      continue;
+    }
+
+    // Keep it
+    it++;
   }
+
+  // // See the rest
+  // printSides();
 }
 
 static int getSurfaceArea()
 {
   for ( Cube cube : cubes ) {
-    double diffs[] = { -0.5, 0.5 };
-    for ( double diff : diffs ) {
-      sides.insert( { (double)cube.x + diff, (double)cube.y, (double)cube.z } );
-      sides.insert( { (double)cube.x, (double)cube.y + diff, (double)cube.z } );
-      sides.insert( { (double)cube.x, (double)cube.y, (double)cube.z + diff } );
+    float diffs[] = { -0.5, 0.5 };
+    for ( float diff : diffs ) {
+      sides.insert( { (float)cube.x + diff, (float)cube.y, (float)cube.z } );
+      sides.insert( { (float)cube.x, (float)cube.y + diff, (float)cube.z } );
+      sides.insert( { (float)cube.x, (float)cube.y, (float)cube.z + diff } );
     }
   }
-  removeSidesBetweenCubes();
+  filterSides();
   return sides.size();
 }
 
@@ -123,5 +243,4 @@ int main()
   fclose( out );
 
   cout << getSurfaceArea() << endl;
-  REPORT( sides.size() / cubes.size() );
 }
