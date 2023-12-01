619512a9dd5e3b3c1bc614142a3f1e3acd79b3a4
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-12-01 01:21:54 -0500

    rearrange: add 2023

---
+++ 2022/problems/newday.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+## Creates a folder for a problem, using $1
+# "template" directory must be in the same directory
+# example: ./newday.sh day1
+
+# Create the folder from template
+cp -r template $1
+cd $1
+
+# Rename hello.cpp
+cp hello.cpp $1.cpp
+mv hello.cpp $1_2.cpp
+
+# Edit the Makefile accordingly
+str="s/hello/$1 $1_2/g"
+sed -i -e "$str" Makefile
+
+# I have no clue why sed created this
+rm Makefile-e
+
+# ignore executables
+echo $1 >> .gitignore
+echo $1_2 >> .gitignore
+
+# Create three empty input and output files
+touch {1,2,3}.{IN,OUT}
