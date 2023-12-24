e7404a1cd1ed5f7925356ace7fc942a140595d68
Osama Albahrani <54853250+osalbahr@users.noreply.github.com>
2023-12-24 06:36:33 -0500

    chore: missing newline at end of file

--- 2023/day08/day08.cpp
+++ 2023/day08/day08.cpp
@@ -9,9 +9,19 @@ using namespace std;
 #define REPORT(X) cout << #X << " = " << (X) << endl
 
 // map node to left/right pair
-using MapDS = unordered_map<string, pair<string, string>>;
+using MapDS = unordered_map<int, pair<int, int>>;
 #define left first
 #define right second
+
+// 'A' = 0 -> 'Z' = 25
+static constexpr int base26ToInt(const char* base26)
+{
+	int ret = 0;
+	for (int i = 2, exp = 0; i >= 0; i--, exp++) {
+		ret += (base26[i] - 'A') * pow(26, exp);
+	}
+	return ret;
+}
 
 static MapDS getMap()
 {
@@ -28,9 +38,9 @@ static MapDS getMap()
 		
 		assert(matches == 3);
 
-		string src(srcPtr);
-		string left(leftPtr);
-		string right(rightPtr);
+		int src = base26ToInt(srcPtr);
+		int left = base26ToInt(leftPtr);
+		int right = base26ToInt(rightPtr);
 
 		mp[src] = {left, right};
 	}
@@ -39,7 +49,7 @@ static MapDS getMap()
 	return mp;
 }
 
-static string move(const string& pos, const MapDS& mp, char next)
+static int move(const int pos, const MapDS& mp, char next)
 {
 	if (next == 'L') {
 		return mp.at(pos).left;
@@ -58,9 +68,10 @@ int main()
 
 	MapDS mp = getMap();
 
-	string pos = "AAA";
+	int pos = base26ToInt("AAA");
 	size_t steps;
-	for (steps = 0; pos != "ZZZ"; steps++) {
+	
+	for (steps = 0; pos != base26ToInt("ZZZ"); steps++) {
 		char next = instructions[ steps % instructions.size() ];
 		pos = move(pos, mp, next);
 	}
